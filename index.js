bf54f6fe8d04f9916b76046ceba73ed9003486a9
ShahlaKamal96 <shahla.kamal19@gmail.com>
2023-01-22 20:44:20 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const returnFirstTwoDrivers = (drivers) => drivers.slice(0, 2);
 const returnLastTwoDrivers = (drivers) => drivers.slice(-2);
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-const createFareMultiplier = (multiplier) => (fee) => fee * multiplier;
+const createFareMultiplier = (multiplier) => (fare) => fare * multiplier;
 const fareDoubler = createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
 
